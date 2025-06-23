35d07aa0a636301219e25469d79359d443f795d6
Fenil-rokad <fenilrokad3@gmail.com>
2025-06-23 22:49:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded", function () {
+  const browserName = navigator.appName;
+  const browserVersion = navigator.appVersion;
+
+  const infoDiv = document.getElementById("browser-info");
+  infoDiv.innerText = "You are using " + browserName + " version " + browserVersion;
+});
